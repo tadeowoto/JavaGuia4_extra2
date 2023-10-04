2c32ae95b53d7d03cc5c9d91d39a652a3a9c5eb7
Tadeo <tadeo.woto@gmail.com>
2023-10-04 10:40:01 -0300

    first classes created

---
+++ EJextra_2/src/VIaje.java
@@ -0,0 +1,20 @@
+
+
+
+
+public class Viaje {
+    
+    private int distCiudad,peajes,kmCiudad;
+    private boolean mismaRuta;
+    private Vehiculo vehiculo;
+    private Combustible combustible;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
